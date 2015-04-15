a7ae5fab2f0d959384cb4ed50543f0e18bd93951
Ankur Gupta <ankur@moorem.com>
2015-04-15 23:47:35 +0530

    Pending Orders API Call

--- lib/ruby/api/esignatur/order.rb
+++ lib/ruby/api/esignatur/order.rb
@@ -1,7 +1,28 @@
+require 'faraday'
+require 'json'
+
+API_URL = "https://api.esignatur.dk"
+
 module Esignatur
 
   class Order
-  
+    
+    def self.pending_orders email
+      conn = Faraday.new(:url => API_URL) do |faraday|
+        faraday.request  :url_encoded
+        faraday.response :logger
+        faraday.adapter  Faraday.default_adapter
+      end
+      
+      response = conn.post do |req|
+        req.url '/Order/Pending'
+        req.headers['Content-Type'] = 'application/json'
+        req.headers['X-Esignatur-Id'] = '1aa6f55d-5724-e411-899d-d89d67640044'
+        req.body = "{ 'Email': '#{email}' }"
+      end
+      puts JSON.parse(response.body)["PendingOrders"]
+    end
+    
   end
 
 end
--- test/esignatur/esignatur_test.rb
+++ test/esignatur/esignatur_test.rb
@@ -4,4 +4,11 @@ class EsignaturTest < Minitest::Test
   def test_exists
     assert Esignatur::Order
   end
+  
+  def test_it_gives_back_pending_orders
+    VCR.use_cassette('pending_orders') do
+      orders = Esignatur::Order.pending_orders("test@noemail.com")
+    end
+  end
+  
 end
