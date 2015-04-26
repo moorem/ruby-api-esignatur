6f663436bc8c98e08f6d4532493fd15f45224dfe
Ankur Gupta <ankur@moorem.com>
2015-04-26 23:51:20 +0530

    Test for order status API call

--- test/esignatur/esignatur_test.rb
+++ test/esignatur/esignatur_test.rb
@@ -16,5 +16,15 @@ class EsignaturTest < Minitest::Test
       assert_kind_of Array, result["PendingOrders"]
     end
   end
+  
+  def test_order_status
+    VCR.use_cassette('order_status') do
+      headers = {'Content-Type' => 'application/json',
+        'X-Esignatur-Id' => '1aa6f55d-5724-e411-899d-d89d67640044'}
+      client = Esignatur::APIClient.new(headers)
+      result = client.order_status("info@kreditmatch.dk", "c00771af-359c-4628-a72d-185d20f4e608")
+      assert_kind_of Hash, result
+    end
+  end
 
 end
