3deba7eb765702abc44bf5a3143ebc190c81b999
Ankur Gupta <ankur@moorem.com>
2015-04-27 00:34:14 +0530

    Moved headers to private method

--- test/esignatur/esignatur_test.rb
+++ test/esignatur/esignatur_test.rb
@@ -8,8 +8,7 @@ class EsignaturTest < Minitest::Test
   
   def test_pending_orders
     VCR.use_cassette('pending_orders') do
-      headers = {'Content-Type' => 'application/json',
-        'X-Esignatur-Id' => '1aa6f55d-5724-e411-899d-d89d67640044'}
+      headers = set_headers
       client = Esignatur::APIClient.new(headers)
       result = client.pending_orders("info@kreditmatch.dk")
       assert_kind_of Hash, result
@@ -19,12 +18,19 @@ class EsignaturTest < Minitest::Test
   
   def test_order_status
     VCR.use_cassette('order_status') do
-      headers = {'Content-Type' => 'application/json',
-        'X-Esignatur-Id' => '1aa6f55d-5724-e411-899d-d89d67640044'}
+      headers = set_headers
       client = Esignatur::APIClient.new(headers)
       result = client.order_status("info@kreditmatch.dk", "c00771af-359c-4628-a72d-185d20f4e608")
       assert_kind_of Hash, result
     end
+  end
+  
+  
+  private
+  
+  def set_headers
+    {'Content-Type' => 'application/json',
+     'X-Esignatur-Id' => '1aa6f55d-5724-e411-899d-d89d67640044'}
   end
 
 end
